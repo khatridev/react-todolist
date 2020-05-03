a20c9ecb687f725a522c2afb99fe34e233270bb7
dev <khatridevk@gmail.com>
2020-05-04 00:58:48 +0530

    Updates

---
+++ precache-manifest.5a323706aa71a90ce5d1ff7a1ad3328f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "701fd2da6e37064c4e8782b65eafae2c",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "ae6b7f5d118103709563",
+    "url": "/react-todolist/static/css/main.3cb47a2c.chunk.css"
+  },
+  {
+    "revision": "f495e36b541302e004bf",
+    "url": "/react-todolist/static/js/2.92d585af.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todolist/static/js/2.92d585af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6b7f5d118103709563",
+    "url": "/react-todolist/static/js/main.c77e1965.chunk.js"
+  },
+  {
+    "revision": "e0db50d6f38e2b5ad310",
+    "url": "/react-todolist/static/js/runtime-main.24e51d31.js"
+  }
+]);
